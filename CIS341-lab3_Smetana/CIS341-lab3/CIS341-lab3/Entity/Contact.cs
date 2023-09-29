7ac816604b743a9ef84431e94a4f035cf0060daf
unknown <asmet081@uwsp.edu>
2023-09-29 13:42:18 -0500

    Lab03 add

---
+++ CIS341-lab3_Smetana/CIS341-lab3/CIS341-lab3/Entity/Contact.cs
@@ -0,0 +1,12 @@
+﻿namespace CIS341_lab3.Entity
+{
+    public class Contact
+    {
+        //Contact Getters and Setters
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Message { get; set; }
+
+
+    }
+}
